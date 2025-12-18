679b294ebd8787cbc1efe107ce883be069d9da00
Ignacio Fernandez Soriano <ignacio@demo2.com>
2025-12-18 08:35:37 -0500

    build: Add timestamp to asset filenames to force new deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Build: 2025-12-18T08:15:00Z
+// Build timestamp
+const BUILD_VERSION = '2025-12-18-08-30';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
